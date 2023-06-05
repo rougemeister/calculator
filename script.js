f774d2800ccaf4c93cff6987de23d1af5c55f3e5
rougemeister <122017023+rougemeister@users.noreply.github.com>
2023-06-05 08:47:49 +0000

    Added style transform to buttons and completed number buttons function

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-const buttons = document.querySelectorAll('.number, .operator');
+const buttons = document.querySelectorAll('.number');
 const clearButton = document.querySelector('.clear');
-const backspaceButton = document.querySelector('.delete')
+const backspaceButton = document.querySelector('.delete');
+const allButtons = document.querySelectorAll('button');
 
-clearButton.addEventListener('click', deleteAll)
+//transform buttons when clicked
+for(let button of allButtons){
+    button.addEventListener('mousedown', (e)=>{
+        e.target.classList.add('transform-button');
+    })
+}
+for(let button of allButtons){
+    button.addEventListener('mouseup', (e)=>{
+        e.target.classList.remove('transform-button');
+    })
+}
 
 function deleteAll (){
         const topScreen = document.querySelector('.top');
         const downScreen = document.querySelector('.down');
         topScreen.textContent = '';
         downScreen.textContent = '';
-        downScreen.style.display = 'block'
-        for(let button of buttons )button.addEventListener('click', addNumbers)
+        for(let button of buttons ){
+        button.addEventListener('click', getNumber)}
     
 }
 
-for(let button of buttons )button.addEventListener('click', addNumbers)
 
-
-function addNumbers (event){
-        let downScreen = document.querySelector('.down');
+function getNumber(event){
         let topScreen = document.querySelector('.top');
-        downScreen.textContent += event.target.textContent;
-        if(downScreen.textContent.length > 8){
-            topScreen.textContent = downScreen.textContent;
-            downScreen.style.display = 'none'
-        }
-        if(downScreen.textContent.length>19){
-            for(let button of buttons)button.removeEventListener('click', addNumbers)
+        topScreen.textContent += event.target.textContent;
+        if(topScreen.textContent.length>19){
+            for(let button of buttons)button.removeEventListener('click', getNumber)
         }
 }
 
 
+clearButton.addEventListener('click', deleteAll);
+for(let button of buttons )button.addEventListener('click', getNumber);
