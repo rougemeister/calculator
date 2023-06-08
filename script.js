17f077200dfb10cb8941feb4464fc5a53973c07a
rougemeister <122017023+rougemeister@users.noreply.github.com>
2023-06-08 11:36:33 +0000

    Refactored button functions

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-const buttons = document.querySelectorAll('.number');
+const buttons = document.querySelectorAll('.number, .operator');
 const clearButton = document.querySelector('.clear');
 const backspaceButton = document.querySelector('.delete');
 const allButtons = document.querySelectorAll('button');
+const equalToButton = document.querySelector('.equals');
+const operators = document.querySelectorAll('.operator');
 
-//transform buttons when clicked
-for(let button of allButtons){
-    button.addEventListener('mousedown', (e)=>{
-        e.target.classList.add('transform-button');
-    })
-}
-for(let button of allButtons){
-    button.addEventListener('mouseup', (e)=>{
-        e.target.classList.remove('transform-button');
-    })
-}
 
-function deleteAll (){
+
+function clearAll (){
         const topScreen = document.querySelector('.top');
         const downScreen = document.querySelector('.down');
         topScreen.textContent = '';
@@ -34,6 +26,117 @@ function getNumber(event){
         }
 }
 
+function backspace(){
+    let topScreen = document.querySelector('.top');
+    const arr = Array.from(topScreen.textContent);
+    arr.pop();
+    topScreen.textContent = arr.join('');
 
-clearButton.addEventListener('click', deleteAll);
 for(let button of buttons )button.addEventListener('click', getNumber);
+}
+
+
+//transform buttons when clicked
+for(let button of allButtons){
+    button.addEventListener('mousedown', (e)=>{
+        e.target.classList.add('transform-button');
+
+        if(e.target.classList.contains('operator')){
+            e.target.classList.remove('transform-button')
+            e.target.classList.add('operator-transform');
+        }else if(e.target.classList.contains('delete')){
+            e.target.classList.remove('transform-button')
+            e.target.classList.add('delete-transform');
+        }else if(e.target.classList.contains('clear')){
+            e.target.classList.remove('transform-button')
+            e.target.classList.add('clear-transform');
+        }else if(e.target.classList.contains('equals')){
+            e.target.classList.remove('transform-button')
+            e.target.classList.add('equals-transform');
+        }
+    
+})}
+for(let button of allButtons){
+    button.addEventListener('mouseup', (e)=>{
+        e.target.classList.remove('transform-button'); 
+        
+        if(e.target.classList.contains('operator')){
+            e.target.classList.remove('operator-transform');
+        }else if(e.target.classList.contains('delete')){
+            e.target.classList.remove('delete-transform');
+        }else if(e.target.classList.contains('clear')){
+            e.target.classList.remove('clear-transform');
+        }else if(e.target.classList.contains('equals')){
+            e.target.classList.remove('equals-transform');
+        }
+    })
+}
+
+equalToButton.addEventListener('click', calculate);
+
+function calculate(){
+    const topScreen = document.querySelector('.top');
+    const downScreen = document.querySelector('.down');
+    
+
+
+    let topScreenContent = topScreen.textContent;
+    switch(true){
+        case topScreenContent.includes('+'):
+            let [addendOne, addendTwo] = topScreenContent.split('+');
+            let sum = operate('+', addendOne, addendTwo);
+            downScreen.textContent = sum;
+            topScreenContent = sum;
+            break;
+        case topScreenContent.includes('-'):
+            let [minuend, subtrahend] = topScreenContent.split('-')
+            difference = operate('-', minuend, subtrahend);
+            downScreen.textContent = difference;
+            break;
+        case topScreenContent.includes('x'):
+            let [multiplicand, multiplier] = topScreenContent.split('x');
+            let product = operate('x', multiplicand, multiplier);
+            downScreen.textContent = product;
+            break;
+        case topScreenContent.includes('÷'):
+            let [dividend, divisor] = topScreenContent.split('÷');
+            let quotient = operate('÷', dividend, divisor);
+            downScreen.textContent = quotient; 
+            
+    }
+}
+
+function operate(operator, firstNum, secondNum){
+    
+    let results;
+    switch(operator){
+        case '+': 
+        results = Number(firstNum) + Number(secondNum);
+        break;
+        case '-': 
+        results = Number(firstNum) - Number(secondNum);
+        break;
+        case 'x':
+        results = Number(firstNum) * Number(secondNum);
+        break;
+        case '÷':
+        results = Number(firstNum) / Number(secondNum);
+    }
+    if(results.toString() === "NaN"){
+       return results = 'Syntax Error'
+    }
+    if(results.toString().length>9){
+        return results = results.toExponential(5)
+    }
+    
+    return results;
+}
+
+
+
+
+  
+for(let button of buttons )button.addEventListener('click', getNumber);
+clearButton.addEventListener('click', clearAll);
+backspaceButton.addEventListener('click', backspace);
+
